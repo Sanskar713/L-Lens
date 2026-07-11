849eaf1c80135bcea5e4e83d687cf314a2573405
Ranjana725 <ranjanabhadouriya252006@gmail.com>
2026-07-11 07:48:25 +0530

    update files code

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/database");
 
 const app = express();
 
-const PORT = 5000;
+// Connect to MongoDB
+connectDB();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
 app.get("/", (req, res) => {
-    res.send("Legal Lens Backend Running Successfully");
+    res.send("🚀 Legal Lens Backend is Running");
 });
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -24,6 +24,51 @@ window.onload = function(){
 
             return false;
         };
-    }
+    
+}
+function openBooking(name,spec,exp,contact,fee){
+
+document.getElementById("bookingModal").style.display="flex";
+
+document.getElementById("lawyerTitle").innerHTML=name;
+
+document.getElementById("lawyerSpec").innerHTML=spec;
+
+document.getElementById("lawyerExp").innerHTML=exp;
+
+document.getElementById("lawyerContact").innerHTML=contact;
+
+document.getElementById("lawyerFee").innerHTML=fee;
+
+}
+
+function closeBooking(){
+
+document.getElementById("bookingModal").style.display="none";
+
+}
+
+function bookNow(){
+
+let client=document.getElementById("clientName").value;
+
+if(client==""){
+
+alert("Please enter your name.");
+
+return;
+
+}
+
+let id="LL"+Math.floor(Math.random()*100000);
+
+alert(
+"✅ Appointment Booked Successfully!\n\nAppointment ID: "
++id
+);
+
+closeBooking();
+
+}
 };
 
